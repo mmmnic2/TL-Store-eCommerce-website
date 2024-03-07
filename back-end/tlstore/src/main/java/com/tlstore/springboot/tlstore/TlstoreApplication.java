4e2873db9a02bbeaabaac3418f98fe8dab3b9910
mmmnic2 <30kfs21@gmail.com>
2024-03-07 22:58:33 +0700

    create folder fe and be

---
+++ back-end/tlstore/src/main/java/com/tlstore/springboot/tlstore/TlstoreApplication.java
@@ -0,0 +1,13 @@
+package com.tlstore.springboot.tlstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlstoreApplication.class, args);
+	}
+
+}
---
+++ back-end/tlstore/src/test/java/com/tlstore/springboot/tlstore/TlstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlstore.springboot.tlstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
